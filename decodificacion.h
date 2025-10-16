005c01d36ab48b62a1f1aee2838296542d5aeee2
sebasarangos1210-sys <sebasarangos1210@gmail.com>
2025-10-16 11:58:29 -0500

    Subir_archivo

---
+++ decodificacion.h
@@ -0,0 +1,9 @@
+#ifndef DECODIFICACION_H
+#define DECODIFICACION_H
+
+#include <string>
+
+std::string decodificar_metodo1(std::string binario, int n);
+std::string decodificar_metodo2(std::string binario, int n);
+
+#endif
