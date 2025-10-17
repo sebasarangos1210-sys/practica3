6c2d8f91ce91701defcded4e97aa93a9b5112f93
sebasarangos1210-sys <sebasarangos1210@gmail.com>
2025-10-17 08:20:05 -0500

    Implementacion_metodo2

--- decodificacion.cpp
+++ decodificacion.cpp
@@ -74,3 +74,26 @@ string decodificar_metodo1(string binario, int n) {
         bloque_anterior_decodificado = bloque_decodificado;
     }
     return resultado;
+    string decodificar_metodo2(string binario, int n) {
+        string resultado = "";
+
+        for (int i = 0; i < binario.length(); i += n) {
+            string bloque = binario.substr(i, n);
+
+            while (bloque.length() < n) {
+                bloque += "0";
+            }
+
+            if (bloque.length() > 1) {
+                string bloque_decodificado = "";
+                bloque_decodificado += bloque[bloque.length() - 1];
+                for (int j = 0; j < bloque.length() - 1; j++) {
+                    bloque_decodificado += bloque[j];
+                }
+                resultado += bloque_decodificado;
+            } else {
+                resultado += bloque;
+            }
+        }
+        return resultado;
+    }
