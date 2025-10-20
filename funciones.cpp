a54bf190a88106cf5b510a8f7024e2799f2c1596
sebasarangos1210-sys <sebasarangos1210@gmail.com>
2025-10-19 22:09:13 -0500

    implementacion_funciones

---
+++ funciones.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "funciones.h"
+
+using namespace std;
+
+string leer_archivo(string nombre_archivo) {
+    ifstream archivo(nombre_archivo, ios::binary);
+    string contenido = "";
+
+    if (!archivo) {
+        cout << "Error: No se pudo abrir " << nombre_archivo << endl;
+        return "";
+    }
+
+    string linea;
+    while (getline(archivo, linea)) {
+        contenido += linea + "\n";
+    }
+
+    archivo.close();
+    return contenido;
+}
+
+void escribir_archivo(string nombre_archivo, string contenido) {
+    ofstream archivo(nombre_archivo, ios::binary);
+
+    if (!archivo) {
+        cout << "Error: No se pudo crear " << nombre_archivo << endl;
+        return;
+    }
+
+    archivo << contenido;
+    archivo.close();
+    cout << "Archivo guardado: " << nombre_archivo << endl;
+}
+
+string texto_a_binario(string texto) {
+    string binario = "";
+
+    for (int i = 0; i < texto.length(); i++) {
+        char c = texto[i];
+        int ascii = (int)c;
+
+        for (int j = 7; j >= 0; j--) {
+            if (ascii & (1 << j)) {
+                binario += "1";
+            } else {
+                binario += "0";
+            }
+        }
+    }
+    return binario;
+}
+
+string binario_a_texto(string binario) {
+    string texto = "";
+
+    for (int i = 0; i < binario.length(); i += 8) {
+        string byte_str = binario.substr(i, 8);
+        int valor = 0;
+
+        for (int j = 0; j < 8; j++) {
+            valor = valor * 2;
+            if (byte_str[j] == '1') {
+                valor += 1;
+            }
+        }
+
+        texto += (char)valor;
+    }
+    return texto;
+}
