0ff86bcb31ceef43b183cba1d8f5eea6b7a35884
sebasarangos1210-sys <sebasarangos1210@gmail.com>
2025-10-19 22:09:38 -0500

    implementacion_funciones

--- funciones.h
+++ funciones.h
@@ -1,4 +1,14 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-#endif // FUNCIONES_H
+#include <string>
+
+// Funciones de archivos
+std::string leer_archivo(std::string nombre_archivo);
+void escribir_archivo(std::string nombre_archivo, std::string contenido);
+
+// Funciones de conversion
+std::string texto_a_binario(std::string texto);
+std::string binario_a_texto(std::string binario);
+
+#endif
